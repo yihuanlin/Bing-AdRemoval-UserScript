efd1c2d33b4db913b2ab87dae00fca8fd3a0a82a
Yi, Huanlin <yhl@yhl.ac.cn>
2023-11-30 03:15:27 +0000

    update

--- bing.adRemoval.user.js
+++ bing.adRemoval.user.js
@@ -10,10 +10,34 @@
 // ==/UserScript==
 
 (function() {
-    document.querySelectorAll(".b_ad").forEach(el => el.remove());
-    document.querySelectorAll(".b_spa_adblock").forEach(el => el.remove());
-    document.getElementById("id_rh").remove()
-    document.getElementById("id_n").remove()
-    document.getElementById("id_nc").remove()
-    document.getElementById("id_p").setAttribute("src", "https://yhl.ac.cn/icon/Avatar.png");
+    try {
+        document.getElementById("id_p").setAttribute("src", "https://yhl.ac.cn/icon/Avatar.png");
+    } catch (error) {
+    }
+    try {
+        document.querySelectorAll(".b_ad").forEach(el => el.remove());
+    } catch (error) {      
+    }
+    try {
+        document.querySelectorAll(".b_spa_adblock").forEach(el => el.remove());
+    } catch (error) {
+    }
+    try {
+        document.getElementById("id_rh").remove();
+    } catch (error) {
+    }
+    try {
+        document.getElementById("id_n").remove();
+    } catch (error) {
+    }
+    try {
+        document.getElementById("id_nc").remove();
+    } catch (error) {
+    }
+    setTimeout(() => {
+        try {
+            document.getElementById("id_p").setAttribute("src", "https://yhl.ac.cn/icon/Avatar.png");
+        } catch (error) {
+        }
+    }, 1000);
 })();
